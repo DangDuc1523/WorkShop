977caa9c21c3d620a57cfcf78ab4b9c04d450fd0
DangDuc1523 <133780382+DangDuc1523@users.noreply.github.com>
2023-06-17 17:59:18 +0700

    Add files via upload

---
+++ WorkShop5/OrganizationManager/src/DTO/Organization.java
@@ -0,0 +1,22 @@
+package DTO;
+
+abstract public class Organization {
+   protected int size;
+   public Organization(){
+   }
+   public Organization(int size){    
+       this.size = size;
+   }
+   public int getSize(){
+    return this.size;
+}
+   public void setSize(int size){
+     this.size = size;
+   }
+   abstract public void communicateByTool();
+     
+@Override
+   public String toString(){
+       return "the organization's size is " + size;
+   }
+}
